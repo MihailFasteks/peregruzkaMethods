ebc12f2485ec7ddba232fd5ec14bf1b7c431948b
MihailFasteks <fastakovskiy.mihail04@gmail.com>
2023-10-01 21:37:58 +0300

    Initial Commit

---
+++ Перегрузка методов/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Перегрузка методов
+//
+//  Created by Michalis on 01.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
